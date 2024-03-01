6a819ae5ff92fce029198f2a35d9230cb6df0fea
GauravSamanta <gauravsamanta143@gmail.com>
2024-03-01 21:40:45 +0530

    added login page -frontend

---
+++ frontend/src/pages/login/Login.jsx
@@ -0,0 +1,17 @@
+
+const Login = () => {
+    return <div className="flex flex-col items-center justify-center min-w-96 mx-auto"
+    >
+<div className="w-full p-6 rounded-lg shadow-md bg-gray-400 bg-clip-padding backdrop-filter bg-opacity-0">
+<h1 className="text-3xl font-semibold text-center text-gray-300 ">
+    Login
+    <span className="text-gray-950"> Sync Chat</span>
+</h1>
+</div>
+
+
+    </div>;
+}
+
+
+export default Login;
